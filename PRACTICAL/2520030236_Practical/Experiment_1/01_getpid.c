aba8504865f4f8d8663b460fdf3449644b9f8c9a
Jashwanthreddy-17 <jashwanthreddy@klh.edu.in>
2026-08-03 09:34:50 +0530

    Add files via upload

---
+++ PRACTICAL/2520030236_Practical/Experiment_1/01_getpid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("PID = %d\n", getpid());
+    return 0;
+}#include<stdio.h>
+#include<unistd.h>
+int main() {
+printf("PID=%d/n", getpid());
+return 0;
+}
